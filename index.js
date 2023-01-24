6e7e895d7e455a49b3081fbd9920b41b4a8a4060
simon codes <simont.codes@gmail.com>
2023-01-24 13:56:50 -0500

    boilerplate backend express and mongoose

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect(process.env.DB_CONNECTOR, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+// Error handling for MongoDB connection
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB");
+});
+
+// Middleware
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors());
+
+// Routes
+
+// Start server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
