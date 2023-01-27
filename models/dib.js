9c03aa50f0b69cc3e987e4a73b2601159876d838
simon codes <simont.codes@gmail.com>
2023-01-27 13:59:19 -0500

    added models for admins and dibs, also connected db to mongo atlas

---
+++ models/dib.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DibsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: [
+      "Fullstack Developer",
+      "Frontend Developer",
+      "Backend Developer",
+      "Project Manager",
+      "QA Tester",
+      "UX/UI",
+      "DevOps",
+    ],
+    default: "user",
+  },
+});
+
+module.exports = Dibs = mongoose.model("dibs", DibsSchema);
